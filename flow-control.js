7e19b23362beb9fd238c8543313d53ee87265753
Glenn Jensen <g.e.jensen@gmail.com>
2020-06-04 19:12:24 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -2,7 +2,7 @@ function basicTeenager(age) {
 
 if(age >= 13 && age <=19) {
   //console.log("You are a teenager!")
-  return "You are a teenager!"
+  return "You are a teenager!";
   /* else {
     console.log("You are not a teenager")
     return "You are not a teenager"
